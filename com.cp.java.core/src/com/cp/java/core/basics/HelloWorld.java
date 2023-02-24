b3b1c01200b354dfb82e83cba767888a388c2404
KONDAPALLI\shashi <kondapallishashi@gmail.com>
2023-02-24 15:05:58 +0530

    Creating a basics branch and pushing the code to basics

--- com.cp.java.core/src/com/cp/java/core/basics/HelloWorld.java
+++ com.cp.java.core/src/com/cp/java/core/basics/HelloWorld.java
@@ -1,3 +1,15 @@
+/*
+ * This program demonstrates a basic java program and attempts to describe the structure of Java program.
+ */
+
+/*	Define a package to organize the code in a program. 
+ * 	The package translates to physical folders in the file system
+ * 	For example the below code is stored in folder structure com-->cp-->java-->core-->basics
+ * 	The package name must be the first ever line in the program, discounting the comments sections
+ * 	using packages also facilitates using different code in similarly named files as long as package names are different.
+ * 	More details in com.cp.java.packages 
+ */
+
 package com.cp.java.core.basics;
 
 public class HelloWorld {
